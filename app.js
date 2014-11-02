e5bda76896e994c222d131973ae36b29ea0a8a6c
Weston Platter <westonplatter@gmail.com>
2014-11-02 16:47:43 -0700

    related to #5. change index.html to reference new js file locations.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 var port = process.env.PORT || 3000;
 
-
 server.listen(port, function() {
   console.log("Server listening on port %d", port);
 });
