7fa867ef7966ef1d665d275bf27fbe873bc25021
Weston Platter <weston.platter@connextions.com>
2014-11-06 12:20:12 -0700

    make it fail

--- specs/app_spec.js
+++ specs/app_spec.js
@@ -1,5 +1,11 @@
 describe("placeholder", function(){
+  
   it("true should be true", function(){
     expect(true).toBe(true);
   });
+
+  it("true should be true", function(){
+    expect(false).toBe(true);
+  });
+
 });
